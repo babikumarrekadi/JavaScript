b6fb678311f86fa67b38eecdaeb315541a08075e
rekadibabikumar <rbabikumar43@gmail.com>
2026-01-30 13:52:30 +0530

    first commit

---
+++ Practice/Day1/var.js
@@ -0,0 +1,43 @@
+// var keyword
+var sub = "var keyword"  //declaration & assigning
+console.log(sub)
+
+var sub = "Babi"       //redeclaration
+console.log(sub)
+
+sub = "kumar"       //reassigning
+console.log(sub) 
+// -----------------------------------------------------------------------------------------
+
+// let ketword
+let intro = "let ketword"      //declaration & assigning  
+console.log(intro) 
+
+// let intro = "kumar"     //redeclaration
+// console.log(intro) 
+
+intro = "Babi"             //reassigning
+console.log(intro)       
+// ----------------------------------------------------------------------------------------
+
+// const keyword
+const num = "const keyword"       //declaration & assigning  
+console.log(num)
+
+// const num = "kumar"      //redeclaration
+// console.log(num)
+
+// num = "kumar"         //reassigning
+// console.log(num)
+
+// -----------------------------------------------------------------------------------------
+
+const namee="Babi"
+var educ="MCA"
+let place="Machilipatanam"
+var year=2
+console.log("I am "+namee+ "I am from " +place+ "and I am currently pursuing my " +year+ " year of "+educ)
+
+// -----------------------------------------------------------------------------------------
+
+
