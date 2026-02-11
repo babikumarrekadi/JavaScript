f17a19bcf80e8a6434f2fa0c05c4149eb3cefbaf
rekadibabikumar <rbabikumar43@gmail.com>
2026-02-11 16:58:49 +0530

    array

---
+++ Practice/D8_array/arra.js
@@ -0,0 +1,57 @@
+// Array Methods :
+//             Array methods are built-in functions used to add, remove, search, modify, and loop through arrays.
+
+
+let mobiles=["oneplus","apple","samsung","vivo","oppo","nokia","docomo","redmi","realme","poco"]
+
+// ➤ Length() – Number of elements in an array.
+console.log(mobiles.length);
+
+// ➤ indexOf()
+console.log(mobiles.indexOf("redmi"));
+
+// ➤ includes()
+console.log(mobiles.includes("apple"));
+
+// ➤ concat()
+b=[5,4]
+console.log(mobiles.concat(b));
+
+// ➤ push() – Add to end
+mobiles.push("iqoo")
+console.log(mobiles);
+
+// ➤ pop() – Remove from end
+mobiles.pop()
+console.log(mobiles);
+
+// ➤ unshift() – Add to beginning
+mobiles.unshift("iqoo")
+console.log(mobiles);
+
+// ➤ shift() – Remove from beginning
+mobiles.shift()
+console.log(mobiles);
+
+
+
+
+// ➤ forEach() – Just loop
+
+// ➤ map() – Creates new array
+
+// ➤ filter() – Filters based on condition
+
+// ➤ reduce() – Reduces to single value
+
+// ➤ find()
+
+// ➤ slice() – Does NOT change original
+
+// ➤ splice() – Changes original
+
+// ➤ join()
+
+// ➤ sort()
+
+
---
+++ Tasks/arrayins/ins.js
@@ -0,0 +1,26 @@
+// Method 1 :
+
+// let invited=prompt("Enter The name of the person").toLowerCase()
+// let party=["babi","kumar","naveen","praveen","ramana","manohar","tarun","basha"]
+// if (party.includes(invited)) {
+//     alert("Welcome to the party") 
+// }else {
+//     alert("😒 You are not Invited")
+// }
+
+// Method 2 :
+// let invited=prompt("Enter The name of the person").toLowerCase()
+// let party=["babi","kumar","naveen","praveen","ramana","manohar","tarun","basha"]
+// for (let i = 0; i < party.length; i++) {
+//     if (invited==party[i]) {
+//        alert("Welcome to the party")
+//        break
+//     }
+//     else{
+//     alert("😒 You are not Invited")
+//     break
+// }
+// }
+
+
+
